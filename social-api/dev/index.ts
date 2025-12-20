dad0f201bb20eb248c8bc0a9e5862d75559c06ed
Ei Maung <eimg@fairwayweb.com>
2025-12-20 09:54:33 +0630

    api auth

--- social-api/dev/index.ts
+++ social-api/dev/index.ts
@@ -1,7 +1,14 @@
 import bcrypt from "bcrypt";
 import { faker } from "@faker-js/faker";
+import jwt from "jsonwebtoken";
 
 async function main() {
+    const content = "some content";
+    const token = jwt.sign(content, "my-secret");
+    console.log(token);
+
+    console.log(jwt.verify(token, "my-secret"));
+
     const password = "apple";
 	console.log("Password: ", password);
 
--- social-api/routes/posts.ts
+++ social-api/routes/posts.ts
@@ -3,7 +3,22 @@ const router = Router();
 
 import { prisma } from "../libs/prisma";
 
-router.get("/", async (req, res) => {
+import jwt from "jsonwebtoken";
+
+router.get("/", async(req, res, next) => {
+    const authorization = req.headers.authorization;
+    const token = authorization?.split(" ")[1];
+    if(!token) {
+        return res.status(401).json({ msg: "access token required" });
+    }
+
+    if(!jwt.verify(token, process.env.JWT_SECRET)) {
+        return res.status(401).json({ msg: "invalid token" });
+    }
+
+    next();
+
+}, async (req, res) => {
     const posts = await prisma.post.findMany({
         orderBy: {
             id: "desc",
